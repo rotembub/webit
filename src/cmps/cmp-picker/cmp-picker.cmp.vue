3be99c3e095410eb67c322b4d0b8a7d306fb59df
shforeal <rivena14798@gmail.com>
2021-12-01 20:32:51 +0200

    clean

--- src/cmps/cmp-picker/cmp-picker.cmp.vue
+++ src/cmps/cmp-picker/cmp-picker.cmp.vue
@@ -50,41 +50,41 @@
 </template>
 
 <script>
-import { Collapse, CollapseItem } from 'element-ui'
-import wapHeader from '../wap-cmps/wap-header.cmp.vue'
-import { cmpService } from '../../services/cmp.service.js'
+  import {Collapse, CollapseItem} from 'element-ui';
+  import wapHeader from '../wap-cmps/wap-header.cmp.vue';
+  import {cmpService} from '../../services/cmp.service.js';
 
-export default {
-  name: 'cmpPicker',
-  components: {
-    wapHeader,
-    Collapse,
-    CollapseItem,
-  },
-  data() {
-    return {
-      themes: null,
-    }
-  },
-  methods: {
-    async add(cmpId) {
-      console.log(cmpId)
-      try {
-        const cmp = await this.$store.dispatch({
-          type: 'addCmp',
-          id: cmpId,
-        })
-      } catch (err) {
-        console.log(err)
-      }
+  export default {
+    name: 'cmpPicker',
+    components: {
+      wapHeader,
+      Collapse,
+      CollapseItem,
     },
-    loadThemes(cmpType) {
-      const allThemes = cmpService.getThemesFor(cmpType)
-      this.themes = allThemes
-      console.log(this.themes)
+    data() {
+      return {
+        themes: null,
+      };
     },
-  },
-}
+    methods: {
+      async add(cmpId) {
+        console.log(cmpId);
+        try {
+          const cmp = await this.$store.dispatch({
+            type: 'addCmp',
+            id: cmpId,
+          });
+        } catch (err) {
+          console.log(err);
+        }
+      },
+      loadThemes(cmpType) {
+        const allThemes = cmpService.getThemesFor(cmpType);
+        this.themes = allThemes;
+        console.log(this.themes);
+      },
+    },
+  };
 </script>
 
 <style></style>
