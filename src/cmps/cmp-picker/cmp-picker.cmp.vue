6cc6795ffe699cda6fa738f124d097dcacf317be
rotem <rotembub@gmail.com>
2021-12-01 23:09:24 +0200

    minor fixes

--- src/cmps/cmp-picker/cmp-picker.cmp.vue
+++ src/cmps/cmp-picker/cmp-picker.cmp.vue
@@ -1,16 +1,32 @@
 <template>
-  <el-collapse class="cmp-picker-collapse">
-    <el-collapse-item title="Headers" @click.native="loadThemes('wap-header')">
+  <el-collapse class="cmp-picker-collapse" accordion>
+    <el-collapse-item
+      v-for="type in types"
+      :title="getProperTxt(type)"
+      :key="'k' + type"
+      @click.native="loadThemes(type)"
+    >
       <div
         v-if="themes"
         v-for="theme in themes"
         :key="theme.type"
         @click.self.stop="add(theme.cmpId)"
       >
-        <!-- <img
+        {{ theme.type }}
+      </div>
+    </el-collapse-item>
+  </el-collapse>
+  <!-- <el-collapse-item title="Headers" @click.native="loadThemes('wap-header')">
+      <div
+        v-if="themes"
+        v-for="theme in themes"
+        :key="theme.type"
+        @click.self.stop="add(theme.cmpId)"
+      > -->
+  <!-- <img
           :src="require('@/assets/cmp-picker-preview/' + theme.type + '.png')"
         /> -->
-        {{ theme.type }}
+  <!-- {{ theme.type }}
       </div>
     </el-collapse-item>
     <el-collapse-item
@@ -48,46 +64,53 @@
       >
         {{ theme.type }}
       </div>
-    </el-collapse-item>
-  </el-collapse>
+    </el-collapse-item> -->
+  <!-- </el-collapse> -->
 </template>
 
 <script>
-  import {Collapse, CollapseItem} from 'element-ui';
-  import wapHeader from '../wap-cmps/wap-header.cmp.vue';
-  import {cmpService} from '../../services/cmp.service.js';
+import { Collapse, CollapseItem } from "element-ui";
+import wapHeader from "../wap-cmps/wap-header.cmp.vue";
+import { cmpService } from "../../services/cmp.service.js";
 
-  export default {
-    name: 'cmpPicker',
-    components: {
-      wapHeader,
-      Collapse,
-      CollapseItem,
+export default {
+  name: "cmpPicker",
+  components: {
+    wapHeader,
+    Collapse,
+    CollapseItem,
+  },
+  data() {
+    return {
+      themes: null,
+      types: ["wap-header", "wap-gallery", "wap-text", "wap-contact"],
+    };
+  },
+  methods: {
+    async add(cmpId) {
+      console.log(cmpId);
+      try {
+        const cmp = await this.$store.dispatch({
+          type: "addCmp",
+          id: cmpId,
+        });
+      } catch (err) {
+        console.log(err);
+      }
     },
-    data() {
-      return {
-        themes: null,
-      };
+    loadThemes(cmpType) {
+      const allThemes = cmpService.getThemesFor(cmpType);
+      this.themes = allThemes;
+      console.log(this.themes);
     },
-    methods: {
-      async add(cmpId) {
-        console.log(cmpId);
-        try {
-          const cmp = await this.$store.dispatch({
-            type: 'addCmp',
-            id: cmpId,
-          });
-        } catch (err) {
-          console.log(err);
-        }
-      },
-      loadThemes(cmpType) {
-        const allThemes = cmpService.getThemesFor(cmpType);
-        this.themes = allThemes;
-        console.log(this.themes);
-      },
+    getProperTxt(type) {
+      console.log(type);
+      const textToShow = type.substring(4);
+      return textToShow.charAt(0).toUpperCase() + textToShow.slice(1);
     },
-  };
+  },
+  computed: {},
+};
 </script>
 
 <style></style>
