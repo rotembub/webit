b4fd5f130d2e4eff88f879dac4b7379d391ad1a6
Yarden Mathieson <yardenm2710@gmail.com>
2021-12-03 17:15:26 +0200

    add fullscrenn feature

--- src/cmps/wap-builder.cmp.vue
+++ src/cmps/wap-builder.cmp.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="wap-builder" v-if="wap">
+  <section v-if="wap">
     <!-- <component v-for="cmp in wap.cmps" :cmp="cmp" :is="cmp.type" :key="cmp.id">
     </component> -->
     <div :class="wap.theme">
@@ -9,6 +9,13 @@
         </slot>
       </wap-dynamic>
     </div>
+    <el-button
+      @click="toggleFullScreen"
+      class="is-full-wap-btn"
+      type="info"
+      :icon="iconToShow"
+      circle
+    ></el-button>
   </section>
 </template>
 
@@ -17,13 +24,26 @@
 import wapDynamic from '../cmps/wap-cmps/wap-dynamic.cmp.vue'
 import wapToolBar from './wap-cmps/wap-tool-bar.cmp.vue'
 export default {
+  data() {
+    return {}
+  },
+
   methods: {
+    toggleFullScreen() {
+      this.$store.dispatch({ type: 'toggleWapFullScreen' })
+    },
     updateWap(updatedWap) {
       console.log(updatedWap, 'got to wapBuilder')
       //try to update here
     },
   },
   computed: {
+    iconToShow() {
+      const isFullScreen = this.$store.getters.isFullScreen
+      if (isFullScreen) return 'el-icon-zoom-out'
+      return 'el-icon-zoom-in'
+    },
+
     wap() {
       // console.log(this.$store.getters.getCurrWap);
       console.log('wapBuilder computed', this.$store.getters.getCurrWap)
--- src/pages/editor-page.vue
+++ src/pages/editor-page.vue
@@ -1,33 +1,43 @@
 <template>
   <section class="editor-page flex">
-    <cmp-picker class="cmp-picker" />
-    <wap-builder class="wap-builder" />
+    <cmp-picker :class="classForPicker" />
+    <wap-builder :class="classForBuilder" />
   </section>
 </template>
 
 <script>
-import wapBuilder from "../cmps/wap-builder.cmp.vue";
-import cmpPicker from "../cmps/cmp-picker/cmp-picker.cmp.vue";
+import wapBuilder from '../cmps/wap-builder.cmp.vue'
+import cmpPicker from '../cmps/cmp-picker/cmp-picker.cmp.vue'
 export default {
   components: {
     wapBuilder,
     cmpPicker,
   },
   data() {
-    return {};
+    return {}
   },
   created() {
-    const id = this.wapId;
+    const id = this.wapId
     // console.log(id);
-    if (!id) this.$store.dispatch({ type: "getEmptyWap" });
-    else this.$store.dispatch({ type: "setCurrWap", wapId: id });
+    if (!id) this.$store.dispatch({ type: 'getEmptyWap' })
+    else this.$store.dispatch({ type: 'setCurrWap', wapId: id })
   },
   computed: {
+    classForPicker() {
+      const isFullScreen = this.$store.getters.isFullScreen
+      if (isFullScreen) return 'cmp-picker-fullscreen'
+      else return 'cmp-picker'
+    },
+    classForBuilder() {
+      const isFullScreen = this.$store.getters.isFullScreen
+      if (isFullScreen) return 'wap-builder-fullscreen'
+      else return 'wap-builder'
+    },
     wapId() {
-      return this.$route.params.wapId;
+      return this.$route.params.wapId
     },
   },
-};
+}
 </script>
 
 <style></style>
