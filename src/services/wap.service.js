7b308c237c06ec678bbbce58f4a3f4d87627fba9
shforeal <rivena14798@gmail.com>
2021-12-03 14:36:54 +0200

    edit for all curr cmps

--- src/services/wap.service.js
+++ src/services/wap.service.js
@@ -1,8 +1,8 @@
-import { storageService } from './async-storage.service'
-import { utilService } from './util.service'
+import {storageService} from './async-storage.service';
+import {utilService} from './util.service';
 
-const KEY = 'wap_DB'
-const CMP_KEY = 'cmp_DB'
+const KEY = 'wap_DB';
+const CMP_KEY = 'cmp_DB';
 
 export const wapService = {
   // add,
@@ -14,7 +14,7 @@ export const wapService = {
   removeCmp,
   addCmp,
   updateCmp,
-}
+};
 
 // More ways to send query params:
 // return axios.get('api/wap/?id=1223&balance=13')
@@ -23,11 +23,11 @@ export const wapService = {
 // Create Test Data:
 
 function createWaps() {
-  const waps = localStorage.getItem(KEY)
+  const waps = localStorage.getItem(KEY);
 
-  storageService.postMany(KEY, [wap_architecture, wap_fylo])
+  storageService.postMany(KEY, [wap_architecture, wap_fylo]);
   // storageService.post(KEY, wap_architecture)
-  console.log('waps found in storage:', waps)
+  console.log('waps found in storage:', waps);
   // if (!waps || !waps.length)
   //   localStorage.setItem(KEY, JSON.stringify([wap_architecture]))
 }
@@ -35,73 +35,73 @@ function createWaps() {
 async function query(filterBy) {
   // console.log('query in services')
   try {
-    const wap = await storageService.query(KEY)
+    const wap = await storageService.query(KEY);
     // if (!wap || !wap.length) return wap_architecture
-    return wap
+    return wap;
   } catch (err) {
-    console.log('couldnt find Waps', err)
+    console.log('couldnt find Waps', err);
   }
 }
 async function getById(id) {
-  console.log(id)
+  console.log(id);
   // if (!id) return getEmptyWap();
   try {
     // console.log('i am still here')
-    const foundWap = await storageService.get(KEY, id)
-    return foundWap
+    const foundWap = await storageService.get(KEY, id);
+    return foundWap;
     // var copiedWap = JSON.parse(JSON.stringify(foundWap))
     // delete copiedWap._id   // when working with mongo be sure to delete the ID entirely before send it to the DB
     // const newWap = save(copiedWap)
     // return newWap;
   } catch (err) {
-    console.log('couldnt get wap by ID', err)
+    console.log('couldnt get wap by ID', err);
   }
 }
 
 async function updateCmp(wapId, newCmp) {
-  console.log('here at Update CMP', wapId, newCmp)
-  var wap = await getById(wapId)
-  const idx = wap.cmps.findIndex(cmp => cmp.id === newCmp.id)
-  wap.cmps.splice(idx, 1, newCmp)
-  console.log(wap)
-  return await save(wap)
+  console.log('here at Update CMP', wapId, newCmp);
+  var wap = await getById(wapId);
+  const idx = wap.cmps.findIndex((cmp) => cmp.id === newCmp.id);
+  wap.cmps.splice(idx, 1, newCmp);
+  console.log(wap);
+  return await save(wap);
 }
 
 // prototype:
 async function removeCmp(wapId, cmpId) {
-  var wap = await getById(wapId)
-  console.log('wap found:', wap)
-  console.log('removing cmp id:', cmpId)
-  const idx = wap.cmps.findIndex(cmp => cmp.id === cmpId)
-  wap.cmps.splice(idx, 1)
-  return await save(wap)
+  var wap = await getById(wapId);
+  console.log('wap found:', wap);
+  console.log('removing cmp id:', cmpId);
+  const idx = wap.cmps.findIndex((cmp) => cmp.id === cmpId);
+  wap.cmps.splice(idx, 1);
+  return await save(wap);
   // return wap;
 }
 // prototype:
 async function addCmp(wapId, cmp) {
-  console.log('adding cmp with ID:', cmp.id)
-  var wap = await getById(wapId)
-  wap.cmps.push(cmp)
-  return await save(wap)
+  console.log('adding cmp with ID:', cmp.id);
+  var wap = await getById(wapId);
+  wap.cmps.push(cmp);
+  return await save(wap);
 }
 
 async function save(wap) {
   try {
     const savedWap = wap._id
       ? await storageService.put(KEY, wap)
-      : await storageService.post(KEY, wap)
-    return savedWap
+      : await storageService.post(KEY, wap);
+    return savedWap;
   } catch (err) {
-    console.log('failed to save wap')
+    console.log('failed to save wap');
   }
 }
 
 async function remove(wapId) {
   // return httpService.delete(`wap/${wapId}`)
   try {
-    return await storageService.delete(KEY, wapId)
+    return await storageService.delete(KEY, wapId);
   } catch (err) {
-    console.log('failed to delete wap', err)
+    console.log('failed to delete wap', err);
   }
 }
 
@@ -131,39 +131,36 @@ async function getEmptyWap() {
       username: '',
     },
     usersData: {
-      contacts: [{ email: '', msg: '', at: null }],
-      signups: [{ email: '', at: null }],
+      contacts: [{email: '', msg: '', at: null}],
+      signups: [{email: '', at: null}],
     },
     cmps: [],
-  }
-  return await save(newWap)
+  };
+  return await save(newWap);
 }
 
 // This IIFE async functions for Dev purposes
 // It allows testing of real time updates (such as sockets) by listening to storage events
-; (async () => {
-  var waps = await storageService.query(KEY)
+// ; (async () => {
+//   var waps = await storageService.query(KEY)
 
-  // Dev Helper: Listens to when localStorage changes in OTHER browser
-  window.addEventListener('storage', async () => {
-    console.log('Storage updated')
-    const freshWaps = await storageService.query(KEY)
-    if (freshWaps.length === waps.length + 1) {
-      console.log('Wap Added - localStorage updated from another browser')
-      socketService.emit(
-        SOCKET_EVENT_REVIEW_ADDED,
-        freshWaps[freshWaps.length - 1]
-      )
-    }
-    waps = freshWaps
-  })
-})()
+//   // Dev Helper: Listens to when localStorage changes in OTHER browser
+//   window.addEventListener('storage', async () => {
+//     console.log('Storage updated')
+//     const freshWaps = await storageService.query(KEY)
+//     if (freshWaps.length === waps.length + 1) {
+//       console.log('Wap Added - localStorage updated from another browser')
+//       socketService.emit(
+//         SOCKET_EVENT_REVIEW_ADDED,
+//         freshWaps[freshWaps.length - 1]
+//       )
+//     }
+//     waps = freshWaps
+//   })
+// })()
 
 // *. add <select> for theme (themes are hard coded inservice.getThemesFor(wapCmp.type))    -- classes?
 //make hardcoded cmps array
-
-
-
 
 const wap_architecture = {
   // _id: 'aaaaa12',
@@ -175,10 +172,8 @@ const wap_architecture = {
     username: 'Hekro Special',
   },
   usersData: {
-    contacts: [
-      { email: 'user@user.com', msg: 'Please send me stuff', at: 123 },
-    ],
-    signups: [{ email: 'user@user.com', at: 123 }],
+    contacts: [{email: 'user@user.com', msg: 'Please send me stuff', at: 123}],
+    signups: [{email: 'user@user.com', at: 123}],
   },
 
   cmps: [
@@ -188,7 +183,7 @@ const wap_architecture = {
       info: {
         title: '',
         subtitle: '',
-        logo: { type: 'txt', txt: 'Utica' },
+        logo: {type: 'txt', txt: 'Utica'},
         navBar: ['Work', 'About', 'Our Team', 'Press', 'Contact'],
       },
       theme: 'theme-header-architecture',
@@ -218,8 +213,9 @@ const wap_architecture = {
       type: 'wap-text',
       info: {
         title: ['Recent Work...'],
-        subtitle:
-          ['Our practice spans from environmental retrofits of existing buildings to the complete planning and design of new neighborhoods and public spaces. While our work is aesthetically diverse, our projects are linked by a focus on enhancing human relationships through'],
+        subtitle: [
+          'Our practice spans from environmental retrofits of existing buildings to the complete planning and design of new neighborhoods and public spaces. While our work is aesthetically diverse, our projects are linked by a focus on enhancing human relationships through',
+        ],
       },
       theme: 'theme-text-architecture',
       style: {
@@ -235,10 +231,10 @@ const wap_architecture = {
         title: '',
         subtitle: '',
         imgs: [
-          { url: 'proj1.jpg', title: 'Project one', txt: '' },
-          { url: 'proj2.jpg', title: 'Project two', txt: '' },
-          { url: 'proj3.jpg', title: 'Project three', txt: '' },
-          { url: 'proj4.jpg', title: 'Project four', txt: '' },
+          {url: 'proj1.jpg', title: 'Project one', txt: ''},
+          {url: 'proj2.jpg', title: 'Project two', txt: ''},
+          {url: 'proj3.jpg', title: 'Project three', txt: ''},
+          {url: 'proj4.jpg', title: 'Project four', txt: ''},
         ],
       },
       theme: 'theme-gallery-architecture',
@@ -266,24 +262,19 @@ const wap_architecture = {
     },
   ],
   isPublic: true,
-}
-
-
+};
 
 const wap_fylo = {
   // _id: '999994123sd12',
   name: 'fylo',
-  imgUrl:
-    '',
+  imgUrl: '',
   createdBy: {
     _id: '',
     username: '',
   },
   usersData: {
-    contacts: [
-      { email: 'user@user.com', msg: 'Please send me stuff', at: 123 },
-    ],
-    signups: [{ email: 'user@user.com', at: 123 }],
+    contacts: [{email: 'user@user.com', msg: 'Please send me stuff', at: 123}],
+    signups: [{email: 'user@user.com', at: 123}],
   },
   theme: 'fylo-main',
 
@@ -294,7 +285,7 @@ const wap_fylo = {
       info: {
         title: '',
         subtitle: '',
-        logo: { type: 'img', url: 'fylo-imgs/logo.svg', txt: '' },
+        logo: {type: 'img', url: 'fylo-imgs/logo.svg', txt: ''},
         navBar: ['Features', 'Team', 'Sign In'],
       },
       theme: 'theme-header-fylo',
@@ -309,9 +300,11 @@ const wap_fylo = {
       type: 'wap-card',
       info: {
         title: ['All your files in one secure location'],
-        subtitle: ['Fylo stores all your most important files in one secure location. Access them wherever you need, share and collaborate with friends family, and co-workers'],
+        subtitle: [
+          'Fylo stores all your most important files in one secure location. Access them wherever you need, share and collaborate with friends family, and co-workers',
+        ],
         imgUrl: 'fylo-imgs/illustration-intro.png',
-        buttons: ['Get Started']
+        buttons: ['Get Started'],
       },
       theme: 'theme-card-fylo-v1',
       style: {
@@ -327,10 +320,26 @@ const wap_fylo = {
         title: '',
         subtitle: '',
         imgs: [
-          { url: 'fylo-imgs/icon-access-anywhere.svg', title: 'Access your files, anywhere', txt: ' The ability to use a smartphone, tablet, or computer to access your account means your files follow you everywhere' },
-          { url: 'fylo-imgs/icon-security.svg', title: 'Security you can trust', txt: ' 2-factor authentication and user-controlled encryption are just a couple of the security features we allow to help secure your files' },
-          { url: 'fylo-imgs/icon-collaboration.svg', title: 'Real-time collaboration', txt: ' Securely share files and folders with friends, family and colleagues for live collaboration. No email attachments required' },
-          { url: 'fylo-imgs/icon-any-file.svg', title: 'Store any type of file', txt: 'Whether you\'re sharing holidays photos or work documents, Fylo has you covered allowing for all file types to be securely stored and shared' },
+          {
+            url: 'fylo-imgs/icon-access-anywhere.svg',
+            title: 'Access your files, anywhere',
+            txt: ' The ability to use a smartphone, tablet, or computer to access your account means your files follow you everywhere',
+          },
+          {
+            url: 'fylo-imgs/icon-security.svg',
+            title: 'Security you can trust',
+            txt: ' 2-factor authentication and user-controlled encryption are just a couple of the security features we allow to help secure your files',
+          },
+          {
+            url: 'fylo-imgs/icon-collaboration.svg',
+            title: 'Real-time collaboration',
+            txt: ' Securely share files and folders with friends, family and colleagues for live collaboration. No email attachments required',
+          },
+          {
+            url: 'fylo-imgs/icon-any-file.svg',
+            title: 'Store any type of file',
+            txt: "Whether you're sharing holidays photos or work documents, Fylo has you covered allowing for all file types to be securely stored and shared",
+          },
         ],
       },
       theme: 'theme-gallery-fylo',
@@ -344,8 +353,10 @@ const wap_fylo = {
       id: 'wc575752',
       type: 'wap-card',
       info: {
-        title: ["Stay productive, wherever you are"],
-        subtitle: ['Never let location be an issue when accessing your files. Fylo has you covered for all of your file storage needs', 'Securely share files and folders with friends, family and colleagues for live collaboration. No email attachments required'
+        title: ['Stay productive, wherever you are'],
+        subtitle: [
+          'Never let location be an issue when accessing your files. Fylo has you covered for all of your file storage needs',
+          'Securely share files and folders with friends, family and colleagues for live collaboration. No email attachments required',
         ],
         // link: 'See how Fylo works',
         imgUrl: 'fylo-imgs/illustration-stay-productive.png',
@@ -362,7 +373,26 @@ const wap_fylo = {
       type: 'wap-review',
       info: {
         quotesImg: 'fylo-imgs/bg-quotes.png',
-        reviews: [{ txt: "Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has become a well-oiled collaboration machine.", name: 'Satish Patel', title: 'Founder & CEO, Huddle', avatar: "fylo-imgs/profile-1.jpg" }, { txt: "Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has become a well-oiled collaboration machine.", name: 'Bruce McKenzie', title: 'Founder & CEO, Huddle', avatar: "fylo-imgs/profile-2.jpg" }, { txt: "Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has become a well-oiled collaboration machine.", name: 'Iva Boyd', title: 'Founder & CEO, Huddle', avatar: "fylo-imgs/profile-3.jpg" }]
+        reviews: [
+          {
+            txt: 'Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has become a well-oiled collaboration machine.',
+            name: 'Satish Patel',
+            title: 'Founder & CEO, Huddle',
+            avatar: 'fylo-imgs/profile-1.jpg',
+          },
+          {
+            txt: 'Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has become a well-oiled collaboration machine.',
+            name: 'Bruce McKenzie',
+            title: 'Founder & CEO, Huddle',
+            avatar: 'fylo-imgs/profile-2.jpg',
+          },
+          {
+            txt: 'Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has become a well-oiled collaboration machine.',
+            name: 'Iva Boyd',
+            title: 'Founder & CEO, Huddle',
+            avatar: 'fylo-imgs/profile-3.jpg',
+          },
+        ],
       },
       theme: 'theme-review-fylo',
       style: {
@@ -376,9 +406,10 @@ const wap_fylo = {
       type: 'wap-signup',
       info: {
         title: 'Get Early Access Today',
-        subtitle: 'It only takes a minute to sign up and our free starter tier is extremely generous. If you have any questions, our support team would be happy to help you',
+        subtitle:
+          'It only takes a minute to sign up and our free starter tier is extremely generous. If you have any questions, our support team would be happy to help you',
         inputTxt: 'Get Started For Free!',
-        btnTxt: 'Sign me up!'
+        btnTxt: 'Sign me up!',
       },
       theme: 'theme-signup-fylo',
       style: {
@@ -386,11 +417,11 @@ const wap_fylo = {
         font: 'Fontush',
         color: 'red',
       },
-    }
-  ]
-}
+    },
+  ],
+};
 
-storageService.post('wap_DB', wap_fylo)
-storageService.post('wap_DB', wap_architecture)
+storageService.post('wap_DB', wap_fylo);
+storageService.post('wap_DB', wap_architecture);
 
-createWaps()
+createWaps();
