f0fdfa0f7507c7f0a9d521f32a9a4ecdc3520ea9
shforeal <rivena14798@gmail.com>
2021-12-05 22:54:50 +0200

    fix style edit

--- src/cmps/wap-cmps/wap-container.cmp.vue
+++ src/cmps/wap-cmps/wap-container.cmp.vue
@@ -16,7 +16,12 @@
 -->
 
 <template>
-  <section v-if="cmp" class="wap-container" :class="cmp.theme">
+  <section
+    v-if="cmp"
+    :style="getCurrStyle"
+    class="wap-container"
+    :class="cmp.theme"
+  >
     <!--Need to see if its fits or not as a class  -->
     <!-- <slot>
       <cmps-tool-bar :id="cmp.id"></cmps-tool-bar>
@@ -61,5 +66,23 @@
       wapImg,
     },
     props: ['cmp'],
+    computed: {
+      getCurrStyle() {
+        console.log('IM HERE Gallery', this.cmp.style);
+        console.log('color', this.cmp.style.color);
+        const style = {
+          color: this.cmp.style.color,
+          fontSize: this.cmp.style.fontSize + 'px', //fix fontSize not change
+          backgroundColor: this.cmp.style.backgroundColor,
+          //new size style
+          height: this.cmp.style.height + 'px', // **problem range too small
+          paddingTop: this.cmp.style.paddingTop + 'px',
+          paddingBottom: this.cmp.style.paddingBottom + 'px',
+          paddingLeft: this.cmp.style.paddingLeft + 'px',
+          paddingRight: this.cmp.style.paddingRight + 'px',
+        };
+        return style;
+      },
+    },
   };
 </script>
