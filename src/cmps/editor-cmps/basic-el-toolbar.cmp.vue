030a769df86cb9ebd0d7f7159c652e4234fcf3a7
Yarden Mathieson <yardenm2710@gmail.com>
2021-12-07 12:33:52 +0200

    edited some bugs

--- src/cmps/editor-cmps/basic-el-toolbar.cmp.vue
+++ src/cmps/editor-cmps/basic-el-toolbar.cmp.vue
@@ -1,23 +1,23 @@
 <template>
-  <section
-    class="basic-el-toolbar"
-    :style="getPos"
-  >
+  <section class="basic-el-toolbar" :style="getPos">
     <button title="Remove" class="tool-element" @click.stop="removeEl">
-      <img src="@/assets/element-editor/trashcan.png" />
+      <!-- <img src="@/assets/element-editor/trashcan.png" /> -->
+      <i class="el-icon-delete"></i>
     </button>
     <button
       title="Font"
       class="tool-element"
       @click.stop="openEditorModal = !openEditorModal"
     >
-      <img src="@/assets/element-editor/font-size.png" />
+      <!-- <img src="@/assets/element-editor/font-size.png" /> -->
+      <i class="el-icon-s-operation"></i>
     </button>
     <!-- <button title="Color" class="tool-element">
       <img src="@/assets/element-editor/color.png" />
     </button> -->
     <button title="Size" class="tool-element">
-      <img src="@/assets/element-editor/size.png" />
+      <!-- <img src="@/assets/element-editor/size.png" /> -->
+      <i class="el-icon-rank"></i>
     </button>
     <element-editor
       v-if="openEditorModal"
@@ -28,32 +28,32 @@
 </template>
 
 <script>
-import elementEditor from "./element-editor-cmp.vue";
+import elementEditor from './element-editor-cmp.vue'
 export default {
-  props: ["id", "elStyle", "cmpId", "pos"],
+  props: ['id', 'elStyle', 'cmpId', 'pos'],
   components: { elementEditor },
   created() {
-    console.log(this.pos);
+    console.log(this.pos)
   },
   data() {
     return {
       openEditorModal: false,
       modalPos: null,
-    };
+    }
   },
   computed: {
     getPos() {
-      const pos =  this.$store.getters.getElSelectedPos;
+      const pos = this.$store.getters.getElSelectedPos
       return { top: pos.y + 'px', left: pos.x + 'px' }
     },
   },
   methods: {
     removeEl() {
-      console.log("emiting for removal");
-      this.$emit("removeEl");
+      console.log('emiting for removal')
+      this.$emit('removeEl')
     },
   },
-};
+}
 </script>
 
 <style></style>
