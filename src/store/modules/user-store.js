acb2b78960bb3eafa3701c8f12f45b6bfd4d72eb
shforeal <rivena14798@gmail.com>
2021-12-09 18:13:41 +0200

    added user login, change store file

---
+++ src/store/modules/user-store.js
@@ -0,0 +1,43 @@
+import {userService} from '@/services/user.service.js';
+
+export default {
+  state: {
+    loggedinUser: null,
+  },
+  getters: {
+    getUser(state) {
+      return state.loggedinUser;
+    },
+  },
+  mutations: {
+    setUser(state, {user}) {
+      state.loggedinUser = user;
+    },
+  },
+  actions: {
+    async login({commit}, {cred}) {
+      try {
+        const user = await userService.login(cred);
+        commit({type: 'setUser', user});
+      } catch (err) {
+        console.log(err);
+      }
+    },
+    async signup({commit}, {cred}) {
+      try {
+        const user = await userService.signup(cred);
+        commit({type: 'setUser', user});
+      } catch (err) {
+        console.log(err);
+      }
+    },
+    async logout({commit}) {
+      try {
+        await userService.logout();
+        commit({type: 'setUser', user: null});
+      } catch (err) {
+        console.log(err);
+      }
+    },
+  },
+};
