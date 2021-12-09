1b9525afdc1759cfa81f7d14adbe3a70f06645ba
Yarden Mathieson <yardenm2710@gmail.com>
2021-12-09 09:25:40 +0200

    aa

--- src/services/util.service.js
+++ src/services/util.service.js
@@ -3,36 +3,36 @@ export const utilService = {
   getRandomInt,
   makeId,
   debounce,
-};
+}
 
 function delay(ms = 1500) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, ms);
-  });
+  return new Promise(resolve => {
+    setTimeout(resolve, ms)
+  })
 }
 
 function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min) + min) //The maximum is exclusive and the minimum is inclusive
 }
 
-function debounce(func, timeout = 300) {
-  let timer;
+function debounce(func, timeout = 3000) {
+  let timer
   return (...args) => {
-    clearTimeout(timer);
+    clearTimeout(timer)
     timer = setTimeout(() => {
-      func.apply(this, args);
-    }, timeout);
-  };
+      func.apply(this, args)
+    }, timeout)
+  }
 }
 
 function makeId(length = 5) {
-  var txt = '';
+  var txt = ''
   var possible =
-    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
   for (var i = 0; i < length; i++) {
-    txt += possible.charAt(Math.floor(Math.random() * possible.length));
+    txt += possible.charAt(Math.floor(Math.random() * possible.length))
   }
-  return txt;
+  return txt
 }
